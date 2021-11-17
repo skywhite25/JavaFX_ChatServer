26d493015142706db52ba14fbb7e4b59e1bc4fda
DICT_DEV7 <DICT_DEV7@DESKTOP-P510VID>
2021-11-17 11:37:28 +0900

    chatServer 완료

--- ChatServer/src/application/Main.java
+++ ChatServer/src/application/Main.java
@@ -1,10 +1,21 @@
 package application;
 	
+import java.net.InetSocketAddress;
 import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Iterator;
 import java.util.Vector;
 import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 
@@ -16,14 +27,103 @@ public class Main extends Application {
 	ServerSocket serverSocket;
 	
 	// 서버를 구동시켜 클라이언트의 연결을 기다리는 메소드
-	public void startServer(String IP, int port) {}
+	public void startServer(String IP, int port) {
+		try {
+			serverSocket = new ServerSocket();
+			serverSocket.bind(new InetSocketAddress(IP, port));
+		} catch (Exception e) {
+			e.printStackTrace();
+			if(!serverSocket.isClosed())
+				stopServer();
+			return;
+		}
+		// 클라이언트가 접속할 때까지 계속 기다리는 쓰레드
+		Runnable thread = new Runnable() {
+			
+			@Override
+			public void run() {
+				while(true) {
+					try {
+						Socket socket = serverSocket.accept();
+						clients.add(new Client(socket));
+						System.out.println("[클라이언트 접속]" + socket.getRemoteSocketAddress() + ": " + Thread.currentThread().getName());
+					} catch (Exception e) {
+						if(!serverSocket.isClosed())
+							stopServer();
+						break;
+					}
+				}
+			}
+		};
+		threadPool = Executors.newCachedThreadPool();
+		threadPool.submit(thread);
+	}
 	
 	// 서버의 작동을 중지시키는 메소드
-	public void stopServer() {}
+	public void stopServer() {
+		try {
+			// 현재 작동 중인 모든 소켓 종료
+			Iterator<Client> iterator = clients.iterator();
+			while(iterator.hasNext()) {
+				Client client = iterator.next();
+				client.socket.close();
+				iterator.remove();
+			}
+			// 서버 소켓 객체 종료
+			if(serverSocket != null && !serverSocket.isClosed())
+				serverSocket.close();
+			// 쓰레드 풀 종료
+			if(threadPool != null && !threadPool.isShutdown())
+				threadPool.shutdown();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 	
 	// UI를 생성하고 실직적으로 프로그램을 작동시키는 메소드
 	@Override
-	public void start(Stage primaryStage) {}
+	public void start(Stage primaryStage) {
+		BorderPane root = new BorderPane();
+		root.setPadding(new Insets(5));
+		
+		TextArea textArea = new TextArea();
+		textArea.setEditable(false);
+		textArea.setFont(new Font("나눔고딕", 15));
+		root.setCenter(textArea);
+		
+		Button togglButton = new Button("시작하기");
+		togglButton.setMaxWidth(Double.MAX_VALUE);
+		BorderPane.setMargin(togglButton, new Insets(1,0,0,0));
+		root.setBottom(togglButton);
+		
+		String IP = "127.0.0.1";
+		int Port = 8080;
+		
+		togglButton.setOnAction(event -> {
+			if(togglButton.getText().equals("시작하기")) {
+				startServer(IP, Port);
+				Platform.runLater(() -> {
+					String message = String.format("[서버시작]\n", IP, Port);
+					textArea.appendText(message);
+					togglButton.setText("종료하기");
+				});
+			} else {
+				stopServer();
+				Platform.runLater(() -> {
+					String message = String.format("[서버종료]\n", IP, Port);
+					textArea.appendText(message);
+					togglButton.setText("시작하기");
+				});
+			}
+			
+		});
+		
+		Scene scene = new Scene(root, 400, 400);
+		primaryStage.setTitle("[채팅 서버]");
+		primaryStage.setOnCloseRequest(event -> stopServer());
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
 	
 	
 	// 프로그램의 진입점
